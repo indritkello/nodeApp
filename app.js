cb38c8191307b611be3b46259fa47582fbdff7ff
Simon Gjetaj <gjetajsimon@gmail.com>
2019-01-16 20:28:15 +0100

    Structure the app and made some changes to support the next features

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const session = require('express-session');
-const request = require('request');
-const qs = require('querystring');
-const url = require('url');
-const randomString = require('randomstring');
+const session = require("express-session");
+const request = require("request");
+const qs = require("querystring");
+const randomString = require("randomstring");
+const path = require("path");
 
-const port = process.env.PORT || 9000;
-const redirect_uri = process.env.HOST + '/redirect';
-
-app.use(express.static('views'));
+app.set("view engine", "ejs");
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(
   session({
     secret: randomString.generate(),
@@ -20,31 +18,31 @@ app.use(
   })
 );
 
-app.get('/', (req, res, next) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res, next) => {
+  res.render("Index");
 });
 
-app.get('/login', (req, res, next) => {
+app.get("/login", (req, res, next) => {
   req.session.csrf_string = randomString.generate();
   const githubAuthUrl =
-    'https://github.com/login/oauth/authorize?' +
+    "https://github.com/login/oauth/authorize?" +
     qs.stringify({
       client_id: process.env.CLIENT_ID,
       // redirect_uri: redirect_uri,
-       state: req.session.csrf_string,
+      state: req.session.csrf_string
       // scope: 'user:email'
     });
   res.redirect(githubAuthUrl);
 });
 
-app.all('/redirect', (req, res) => {
+app.all("/redirect", (req, res) => {
   const code = req.query.code;
   const returnedState = req.query.state;
   if (req.session.csrf_string === returnedState) {
     request.post(
       {
         url:
-          'https://github.com/login/oauth/access_token?' +
+          "https://github.com/login/oauth/access_token?" +
           qs.stringify({
             client_id: process.env.CLIENT_ID,
             client_secret: process.env.CLIENT_SECRET,
@@ -55,42 +53,48 @@ app.all('/redirect', (req, res) => {
       },
       (error, response, body) => {
         req.session.access_token = qs.parse(body).access_token;
-        res.redirect('/user');
+        res.redirect("/user");
       }
     );
   } else {
-    res.redirect('/');
+    res.redirect("/");
   }
 });
 
-app.get('/user', (req, res) => {
+app.get("/user", (req, res) => {
   request.get(
     {
-      url: 'https://api.github.com/user/repos',
+      url: "https://api.github.com/user/repos",
       headers: {
-        Authorization: 'token ' + req.session.access_token,
-        'User-Agent': 'Kello'
+        Authorization: "token " + req.session.access_token,
+        "User-Agent": "Kello"
       }
     },
     (error, response, body) => {
-        htmlContent = "<div style=\"float:center\"><ol>";
-        var tabela = JSON.parse(body);
-        
-        for(i=0;i<tabela.length; i++)
-        {
-        htmlContent+= "<li>"+tabela[i].full_name+"</li>";
-        }
-        htmlContent+="</ol></div>";
-      res.send(
-        "<center><h2>You're logged in! Here's all your repositories on GitHub: </h2></center>" +
-          htmlContent +
-          '<center><p>Go back to <a href="./">log in page</a>.</p><center>'+
-          "<h4>JSON Format Response</h4>" + body
-      );
+      // htmlContent = '<div style="float:center"><ol>';
+      // var tabela = JSON.parse(body);
+
+      // for (i = 0; i < tabela.length; i++) {
+      //   htmlContent += "<li>" + tabela[i].full_name + "</li>";
+      // }
+      // htmlContent += "</ol></div>";
+      // res.send(
+      //   "<center><h2>You're logged in! Here's all your repositories on GitHub: </h2></center>" +
+      //     htmlContent +
+      //     '<center><p>Go back to <a href="./">log in page</a>.</p><center>' +
+      //     "<h4>JSON Format Response</h4>" +
+      //     body
+      // );
+
+      let htmlContent = `<div class="container"><div id="profile"></div></div>`;
+      res.send(body);
     }
   );
 });
 
+const port = process.env.PORT || 9000;
+const redirect_uri = process.env.HOST + "/redirect";
+
 app.listen(port, () => {
-  console.log('Server listening at port ' + port);
+  console.log("Server listening at port " + port);
 });
